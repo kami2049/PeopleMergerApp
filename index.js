a6da2679442a0a857140f16415d04f085b004833
kamilah hamilton <hamilton8186@aol.com>
2017-11-01 03:13:19 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+ 
+
+
+
+// fs.readFile('people2.json', 'utf8', (err, data) => {
+// 	if (err) throw err;
+// 	console.log(data);
+// });
+
+// fs.writeFile('test.txt', 'utf8', (err, data) => {
+// 	if (err) throw err;
+// 	console.log(data);
+// });
+
+
